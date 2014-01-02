9a36c8d19caba021bc20933f38bc3abb7c79e6bf
Matti R Pulli <mattipul@svm-27.cs.helsinki.fi>
2014-01-02 14:11:46 +0200

    ..

---
+++ lib/gcc/i586-elf/4.8.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --target=i586-elf --prefix=/cs/fs2/home/mattipul/gcc --disable-nls --enable-languages=c --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "pentium" }, { "arch", "pentium" } };
